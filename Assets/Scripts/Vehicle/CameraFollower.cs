24ea15839c341a7c6d3b688938ef1094f5f04680
Radu Serban <serban@wisc.edu>
2020-05-22 12:34:22 +0200

    Add option to select world up or vehicle up for chase cam

--- Assets/Scripts/Vehicle/CameraFollower.cs
+++ Assets/Scripts/Vehicle/CameraFollower.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    public Transform target;
+    public UChVehicle target;
+
+    public bool worldUp;
+
+    [Range (0.0f, 1.0f)]
     public float smoothSpeed = 0.1f;
 
     [Range (-2.0f, 10.0f)]
@@ -15,12 +19,28 @@ public class CameraFollower : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + target.right * (-cameraDistance) + target.up * cameraHeight;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        //// TODO: worldUp option requires more work...
+        
+        var target_xform = target.transform;
 
-        Quaternion desiredRotation = target.rotation * Quaternion.Euler(0, 90, 0);
-        Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
-        transform.rotation = smoothedRotation;
+        if (worldUp)  // Camera vertical == World vertical
+        {
+            Vector3 target_point = target_xform.TransformPoint(new Vector3(0.5f, 0, 0));
+            transform.LookAt(target_point);
+
+            Vector3 desiredPosition = target_xform.position + target_xform.right * (-cameraDistance) + target_xform.up * cameraHeight;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+        else          // Camera vertical == Vehicle vertical
+        {
+            Vector3 desiredPosition = target_xform.position + target_xform.right * (-cameraDistance) + target_xform.up * cameraHeight;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+
+            Quaternion desiredRotation = target_xform.rotation * Quaternion.Euler(0, 90, 0);
+            Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
+            transform.rotation = smoothedRotation;
+        }
     }
 }
