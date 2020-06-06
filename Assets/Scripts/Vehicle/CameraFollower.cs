ca1bf94f073627df2ac6a65b3a3fdd601c64b728
Radu Serban <serban@wisc.edu>
2020-06-06 12:16:43 +0200

    Relocate additional utility vehicle prefabs to Assets/Resources/

--- Assets/Scripts/Vehicle/CameraFollower.cs
+++ Assets/Scripts/Vehicle/CameraFollower.cs
@@ -95,36 +95,36 @@ public class CameraFollower : MonoBehaviour, IAdvance
 
     private void OnGUI()
     {
-        if (Application.isEditor)
+        if (vehicle == null)
+            return;
+
+        double speedKPH = Math.Round(3.6 * vehicle.GetSpeed());
+        GUI.Label(new Rect(10, 10, 200, 40), "Speed (km/h): " + speedKPH.ToString(), guiStyle);
+        double throttle = Math.Round(vehicle.GetThrottleInput() * 100) / 100;
+        GUI.Label(new Rect(10, 40, 200, 40), "Throttle: " + throttle.ToString(), guiStyle);
+        double braking = Math.Round(vehicle.GetBrakingInput() * 100) / 100;
+        GUI.Label(new Rect(10, 60, 200, 40), "Braking: " + braking.ToString(), guiStyle);
+        double steering = Math.Round(vehicle.GetSteeringInput() * 100) / 100;
+        GUI.Label(new Rect(10, 80, 200, 40), "Steering: " + steering.ToString(), guiStyle);
+
+        switch (vehicle.GetChVehicle().GetPowertrain().GetDriveMode())
         {
-            double speedKPH = Math.Round(3.6 * vehicle.GetSpeed());
-            GUI.Label(new Rect(10, 10, 200, 40), "Speed (km/h): " + speedKPH.ToString(), guiStyle);
-            double throttle = Math.Round(vehicle.GetThrottleInput() * 100) / 100;
-            GUI.Label(new Rect(10, 40, 200, 40), "Throttle: " + throttle.ToString(), guiStyle);
-            double braking = Math.Round(vehicle.GetBrakingInput() * 100) / 100;
-            GUI.Label(new Rect(10, 60, 200, 40), "Braking: " + braking.ToString(), guiStyle);
-            double steering = Math.Round(vehicle.GetSteeringInput() * 100) / 100;
-            GUI.Label(new Rect(10, 80, 200, 40), "Steering: " + steering.ToString(), guiStyle);
-
-            switch (vehicle.GetChVehicle().GetPowertrain().GetDriveMode())
-            {
-                case ChPowertrain.DriveMode.FORWARD:
-                    GUI.Label(new Rect(10, 100, 200, 40), "Gear: Forward", guiStyle);
-                    break;
-                case ChPowertrain.DriveMode.REVERSE:
-                    GUI.Label(new Rect(10, 100, 200, 40), "Gear: Reverse", guiStyle);
-                    break;
-            }
-
-            double motorTorque = Math.Round(vehicle.GetPowertrain().GetMotorTorque());
-            GUI.Label(new Rect(10, 130, 200, 40), "Motor Torque (Nm): " + motorTorque.ToString(), guiStyle);
-            double motorSpeed = Math.Round(vehicle.GetPowertrain().GetMotorSpeed() * 60 / (2 * Math.PI));
-            GUI.Label(new Rect(10, 150, 200, 40), "Motor Speed (RPM): " + motorSpeed.ToString(), guiStyle);
-
-            float wallTime = Time.realtimeSinceStartup;
-            float gameTime = Time.unscaledTime;
-            float ratio = gameTime / wallTime;
-            GUI.Label(new Rect(10, 170, 200, 40), "Time factor: " + Mathf.Round(ratio * 100) / 100, guiStyle);
+            case ChPowertrain.DriveMode.FORWARD:
+                GUI.Label(new Rect(10, 100, 200, 40), "Gear: Forward", guiStyle);
+                break;
+            case ChPowertrain.DriveMode.REVERSE:
+                GUI.Label(new Rect(10, 100, 200, 40), "Gear: Reverse", guiStyle);
+                break;
         }
+
+        double motorTorque = Math.Round(vehicle.GetPowertrain().GetMotorTorque());
+        GUI.Label(new Rect(10, 130, 200, 40), "Motor Torque (Nm): " + motorTorque.ToString(), guiStyle);
+        double motorSpeed = Math.Round(vehicle.GetPowertrain().GetMotorSpeed() * 60 / (2 * Math.PI));
+        GUI.Label(new Rect(10, 150, 200, 40), "Motor Speed (RPM): " + motorSpeed.ToString(), guiStyle);
+
+        float wallTime = Time.realtimeSinceStartup;
+        float gameTime = Time.unscaledTime;
+        float ratio = gameTime / wallTime;
+        GUI.Label(new Rect(10, 170, 200, 40), "Time factor: " + Mathf.Round(ratio * 100) / 100, guiStyle);
     }
 }
