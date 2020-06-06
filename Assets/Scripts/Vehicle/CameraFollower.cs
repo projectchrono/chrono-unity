27b42ff3b0e46937736718447e357c4c5b81be53
Radu Serban <serban@wisc.edu>
2020-06-06 13:54:21 +0200

    Allow chase-cam to detach from any vehicle (Home key toggle)

--- Assets/Scripts/Vehicle/CameraFollower.cs
+++ Assets/Scripts/Vehicle/CameraFollower.cs
@@ -24,10 +24,17 @@ public class CameraFollower : MonoBehaviour, IAdvance
     //public Color textColor;
     private GUIStyle guiStyle = new GUIStyle();
 
+    private Vector3 homePos;
+    private Quaternion homeRot;
+    bool attached = false;
+
     public void Awake()
     {
         guiStyle.fontStyle = FontStyle.Bold;
         guiStyle.normal.textColor = Color.yellow;
+
+        homePos = transform.position;
+        homeRot = transform.rotation;
     }
 
     public void Start()
@@ -39,7 +46,10 @@ public class CameraFollower : MonoBehaviour, IAdvance
         // Initialize list of vehicles currently in the scene and attach to first one if any
         vehicles = FindObjectsOfType<UChVehicle>();
         if (vehicles.Length > 0)
+        {
             vehicle = vehicles[target_index];
+            attached = true;
+        }
     }
 
     public void Update()
@@ -50,20 +60,38 @@ public class CameraFollower : MonoBehaviour, IAdvance
             vehicles = FindObjectsOfType<UChVehicle>();
             int numVehicles = vehicles.Length;
             if (numVehicles == 0)
+            {
+                attached = false;
                 return;
+            }
             if (Input.GetKeyDown(KeyCode.PageUp))
                 target_index = (target_index + 1) % numVehicles;
             if (Input.GetKeyDown(KeyCode.PageDown))
                 target_index = (target_index + numVehicles - 1) % numVehicles;
 
             vehicle = vehicles[target_index];
+            attached = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Home))
+        {
+            if (!attached && vehicle != null)
+            {
+                attached = true;
+            }
+            else if (attached)
+            {
+                transform.position = homePos;
+                transform.rotation = homeRot;
+                attached = false;
+            }
         }
     }
 
     public void Advance(double step)
     {
         // Do nothing if no associated vehicle
-        if (vehicle == null)
+        if (!attached)
             return;
 
         ////Debug.Log("advance ChaseCam. step = " + step);
@@ -95,36 +123,38 @@ public class CameraFollower : MonoBehaviour, IAdvance
 
     private void OnGUI()
     {
-        if (vehicle == null)
+        if (!attached)
             return;
 
+        GUI.Label(new Rect(10, 10, 200, 40), vehicle.name, guiStyle);
+
         double speedKPH = Math.Round(3.6 * vehicle.GetSpeed());
-        GUI.Label(new Rect(10, 10, 200, 40), "Speed (km/h): " + speedKPH.ToString(), guiStyle);
+        GUI.Label(new Rect(10, 40, 200, 40), "Speed (km/h): " + speedKPH.ToString(), guiStyle);
         double throttle = Math.Round(vehicle.GetThrottleInput() * 100) / 100;
-        GUI.Label(new Rect(10, 40, 200, 40), "Throttle: " + throttle.ToString(), guiStyle);
+        GUI.Label(new Rect(10, 70, 200, 40), "Throttle: " + throttle.ToString(), guiStyle);
         double braking = Math.Round(vehicle.GetBrakingInput() * 100) / 100;
-        GUI.Label(new Rect(10, 60, 200, 40), "Braking: " + braking.ToString(), guiStyle);
+        GUI.Label(new Rect(10, 90, 200, 40), "Braking: " + braking.ToString(), guiStyle);
         double steering = Math.Round(vehicle.GetSteeringInput() * 100) / 100;
-        GUI.Label(new Rect(10, 80, 200, 40), "Steering: " + steering.ToString(), guiStyle);
+        GUI.Label(new Rect(10, 110, 200, 40), "Steering: " + steering.ToString(), guiStyle);
 
         switch (vehicle.GetChVehicle().GetPowertrain().GetDriveMode())
         {
             case ChPowertrain.DriveMode.FORWARD:
-                GUI.Label(new Rect(10, 100, 200, 40), "Gear: Forward", guiStyle);
+                GUI.Label(new Rect(10, 130, 200, 40), "Gear: Forward", guiStyle);
                 break;
             case ChPowertrain.DriveMode.REVERSE:
-                GUI.Label(new Rect(10, 100, 200, 40), "Gear: Reverse", guiStyle);
+                GUI.Label(new Rect(10, 130, 200, 40), "Gear: Reverse", guiStyle);
                 break;
         }
 
         double motorTorque = Math.Round(vehicle.GetPowertrain().GetMotorTorque());
-        GUI.Label(new Rect(10, 130, 200, 40), "Motor Torque (Nm): " + motorTorque.ToString(), guiStyle);
+        GUI.Label(new Rect(10, 160, 200, 40), "Motor Torque (Nm): " + motorTorque.ToString(), guiStyle);
         double motorSpeed = Math.Round(vehicle.GetPowertrain().GetMotorSpeed() * 60 / (2 * Math.PI));
-        GUI.Label(new Rect(10, 150, 200, 40), "Motor Speed (RPM): " + motorSpeed.ToString(), guiStyle);
+        GUI.Label(new Rect(10, 180, 200, 40), "Motor Speed (RPM): " + motorSpeed.ToString(), guiStyle);
 
         float wallTime = Time.realtimeSinceStartup;
         float gameTime = Time.unscaledTime;
         float ratio = gameTime / wallTime;
-        GUI.Label(new Rect(10, 170, 200, 40), "Time factor: " + Mathf.Round(ratio * 100) / 100, guiStyle);
+        GUI.Label(new Rect(10, 200, 200, 40), "Time factor: " + Mathf.Round(ratio * 100) / 100, guiStyle);
     }
 }
