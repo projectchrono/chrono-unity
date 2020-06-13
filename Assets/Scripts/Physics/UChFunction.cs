bf36017a4b66f2fa1f9e0ef31ed1f42a26e5ebf2
Radu Serban <serban@wisc.edu>
2020-06-13 16:15:20 +0200

    Remove unused namespaces

--- Assets/Scripts/Physics/UChFunction.cs
+++ Assets/Scripts/Physics/UChFunction.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.AnimatedValues;
 
 public class UChFunction : MonoBehaviour
 {
--- Assets/Scripts/Vehicle/UChTerrain.cs
+++ Assets/Scripts/Vehicle/UChTerrain.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using System;
 
 public class UFlatTerrain : ChTerrain
 {
