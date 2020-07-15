cc4a5ded5d936719e3ea28c0fe2edb5be1b4bb9b
Radu Serban <serban@wisc.edu>
2020-07-15 20:17:11 +0200

    Add simple demo selection scene for various Chrono::Vehicle demos

---
+++ Assets/Scripts/SceneSelect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneSelectData
+{
+    public static bool launchedFromManager = false;
+    public static string mainScene;
+}
+
+public class SceneSelect : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneSelectData.mainScene = SceneManager.GetActiveScene().name;
+        SceneSelectData.launchedFromManager = true;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (SceneManager.GetActiveScene().name == SceneSelectData.mainScene)
+            {
+                Application.Quit();
+            }
+
+            if (SceneSelectData.launchedFromManager)
+            {
+                SceneSelectData.launchedFromManager = false;
+                SceneManager.LoadScene(SceneSelectData.mainScene);
+            }
+        }
+    }
+
+    // When attaching to a Game Object, hide the transform
+    void OnValidate()
+    {
+        transform.hideFlags = HideFlags.NotEditable | HideFlags.HideInInspector;
+    }
+
+}
