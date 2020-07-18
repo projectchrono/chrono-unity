2a655bd490b316edc5a3637d575e5ebc871a50fd
Radu Serban <serban@wisc.edu>
2020-07-18 11:33:42 +0200

    Modify TerrainPatch so that it can optionally create a Chrono collision shape (and thus work with rigid tires)

--- Assets/Scripts/Vehicle/UChTerrainPatch.cs
+++ Assets/Scripts/Vehicle/UChTerrainPatch.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class UChTerrainPatch : MonoBehaviour
 {
-    public float coefficientFriction; 
+    public bool hasContactShape;
+
+    public float coefficientFriction;  // used when no contact shape present
+    private ChMaterialSurface mat;     // used when contact shape present
+    private ChBody body;
+
     private Vector3 normal;  // normal to patch top surface
     private Vector3 center;  // center of patch top surface
     private float radius;    // radius of bounding sphere
@@ -12,24 +17,56 @@ public class UChTerrainPatch : MonoBehaviour
     public UChTerrainPatch()
     {
         coefficientFriction = 0.9f;
+        hasContactShape = false;
     }
 
     void Awake()
     {
-        float thickness = transform.localScale.y;
+        ////Debug.Log(name + "  Create contact shape? " + createContactShape);
+
+        var pos = transform.position;
+        var rot = transform.rotation;
+        var size = transform.localScale;
+
+        float thickness = size.y;
         normal = transform.up;
         center = transform.position + (0.5f * thickness) * normal;
-
         ////Debug.Log("Center: " + center.ToString("F4"));
         ////Debug.Log("Normal: " + normal.ToString("F4"));
+
+        if (hasContactShape)
+        {
+            var mat_component = this.GetComponent<UChMaterialSurface>();
+            mat = mat_component.mat_info.CreateMaterial(mat_component.contact_method);
+
+            body = new ChBody();
+            body.SetBodyFixed(true);
+            body.SetCollide(true);
+            body.SetPos(Utils.ToChronoFlip(pos));
+            body.SetRot(Utils.ToChronoFlip(rot));
+            body.GetCollisionModel().ClearModel();
+            body.GetCollisionModel().AddBox(mat, size.x * 0.5, size.y * 0.5, size.z * 0.5);
+            body.GetCollisionModel().BuildModel();
+        }
 
         var rend = GetComponent<Renderer>();
         radius = rend.bounds.extents.magnitude;
     }
 
+    private void Start()
+    {
+        // If present, add the rigid body to the Chrono system
+        if (hasContactShape)
+            UChSystem.chrono_system.AddBody(body);
+    }
+
     public ChVectorD GetNormal() { return Utils.ToChronoFlip(normal); }
 
-    public float GetCoefficientFriction() { return coefficientFriction; }
+    public float GetCoefficientFriction() {
+        if (hasContactShape)
+            return mat.GetKfriction();
+        return coefficientFriction; 
+    }
 
     public bool Project(ChVectorD loc, out double height)
     {
