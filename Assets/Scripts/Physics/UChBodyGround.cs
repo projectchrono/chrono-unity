737f00149f87efae9389029ac4420d6d3ea6972e
Radu Serban <serban@wisc.edu>
2024-05-27 13:38:22 +0200

    Tweak references to the license file in comments at the top of C# scripts

--- Assets/Scripts/Physics/UChBodyGround.cs
+++ Assets/Scripts/Physics/UChBodyGround.cs
@@ -5,8 +5,7 @@
 // All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found
-// in the LICENSE file at the top level of the distribution and at
-// http://projectchrono.org/license-chrono.txt.
+// in the LICENSE file at the top level of the distribution.
 //
 // =============================================================================
 // Authors: Radu Serban
